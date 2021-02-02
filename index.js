09dd0d718baada378656c55d466e3e6982968efa
edlee <edlee@synapsoft.co.kr>
2021-02-02 21:19:33 +0900

    게시판 뼈대 + 회원가입

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://admin:lshlow1004@cluster0.2wzx7.mongodb.net/nodejs?retryWrites=true&w=majority');
+var db = mongoose.connection;
+db.once('open', function () {
+    console.log('DB connected');
+});
+db.on('error', function (err) {
+    console.log('DB ERROR : ', err);
+});
+
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/posts', require('./routes/posts'));
+app.use('/users', require('./routes/users'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function () {
+    console.log('server on! http://localhost:' + port);
+});
