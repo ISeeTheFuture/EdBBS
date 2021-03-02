6381b60445b8e5093111ed844161dbe9dfcfbe49
edlee <edlee@synapsoft.co.kr>
2021-03-02 23:57:13 +0900

    필요없는 코드 제거

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,11 +1,6 @@
 var showSourceCode = false;
 var isEditMode = true;
 
-user_pref("capability.policy.policynames", "allowclipboard");
-user_pref("capability.policy.allowclipboard.sites", "https://www.mozilla.org");
-user_pref("capability.policy.allowclipboard.Clipboard.cutcopy", "allAccess");
-user_pref("capability.policy.allowclipboard.Clipboard.paste", "allAccess");
-
 function enableEditMode() {
     richTextField.document.designMode = 'On';
 }
